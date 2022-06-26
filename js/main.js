ec199319a62a0319f8b262e1e941ebe500f540d0
gugolm <gugolm@gugolm.com>
2022-06-26 12:18:46 +1000

    Отредактирован вывод в консоль.

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ const terminalContainer = document.getElementById('terminal');
 const sendForm = document.getElementById('send-form');
 const inputField = document.getElementById('input');
 
+let flag = 'None';
+
 // Helpers.
 const defaultDeviceName = 'Lights';
 const terminalAutoScrollingLimit = terminalContainer.offsetHeight / 2;
@@ -36,7 +38,19 @@ const terminal = new BluetoothTerminal();
 
 // Override `receive` method to log incoming data to the terminal.
 terminal.receive = function(data) {
-  logToTerminal(data, 'in');
+  // if (data.includes('OK+Set:')) {
+  //   logToTerminal('Setted parameter ' + data, 'in');}
+  if (flag == 'set_parameter') {
+    if (data == 'OK+Set:200')
+      logToTerminal('Гирлянда включена ' + data, 'sys');
+    else if (data == 'OK+Set:000')
+      logToTerminal('Гирлянда выключена ' + data, 'sys');
+    else if (data == 'OK+RESET')
+      logToTerminal('Устройство перезагружается ' + data, 'sys');}
+  else if (flag == 'battery_voltage') {
+    logToTerminal('Напряжение батареи ' + data, 'sys');}
+  else {
+    logToTerminal('Received data ' + data, 'in');}
 };
 
 // Override default log method to output messages to the terminal and console.
@@ -50,9 +64,38 @@ terminal._log = function(...messages) {
 
 // Implement own send function to log outcoming data to the terminal.
 const send = (data) => {
-  terminal.send(data).
-      then(() => logToTerminal(data, 'out')).
-      catch((error) => logToTerminal(error));
+    flag = 'None';
+    terminal.send(data).
+    then(() => logToTerminal(data, 'out')).
+    catch((error) => logToTerminal(error));
+};
+
+// Implement own send function to log outcoming data to the terminal.
+const sendcmd = (data) => {
+  if (data == 'AT+BATT?') {
+    terminal.send(data).
+    // then(() => logToTerminal('Напряжение батареи', 'out')).
+    catch((error) => logToTerminal(error));}
+  else if (data == 'AT+RESET') {
+    // flag = 'set_parameter';
+    terminal.send(data).
+    // then(() => logToTerminal('Устройство перезагружается', 'out')).
+    catch((error) => logToTerminal(error));}
+  else if (data == 'AT+BEFC200') {
+    // flag = 'set_parameter';
+    terminal.send(data).
+    // then(() => logToTerminal('Гирлянда включена', 'out')).
+    catch((error) => logToTerminal(error));}
+  else if (data == 'AT+BEFC000') {
+    // flag = 'set_parameter';
+    terminal.send(data).
+    // then(() => logToTerminal('Гирлянда выключена', 'out')).
+    catch((error) => logToTerminal(error));}
+  else {
+    // flag = 'None';
+    terminal.send(data).
+    // then(() => logToTerminal('Leds OFF', 'out')).
+    catch((error) => logToTerminal(error));}    
 };
 
 // Bind event listeners to the UI elements.
@@ -71,32 +114,36 @@ disconnectButton.addEventListener('click', () => {
 
 ledonButton.addEventListener(
   'click', () => {
-    send('AT+BEFC200');
-    setTimeout(() => {  send('AT+AFTC200'); }, 200);
+    flag = 'set_parameter';
+    sendcmd('AT+BEFC200');
+    setTimeout(() => {  sendcmd('AT+AFTC200'); }, 200);
     // setTimeout(() => {  send('AT+PIO21'); }, 1000);
-    setTimeout(() => {  send('AT+RESET'); }, 400);
+    setTimeout(() => {  sendcmd('AT+RESET'); }, 400);
   }
 );
 
 ledoffButton.addEventListener(
   'click', () => {
-    send('AT+BEFC000');
-    setTimeout(() => {  send('AT+AFTC000'); }, 200);
+    flag = 'set_parameter';
+    sendcmd('AT+BEFC000');
+    setTimeout(() => {  sendcmd('AT+AFTC000'); }, 200);
     // setTimeout(() => {  send('AT+PIO20'); }, 1000);
-    setTimeout(() => {  send('AT+RESET'); }, 400);
+    setTimeout(() => {  sendcmd('AT+RESET'); }, 400);
   }
 );
 
 batteryButton.addEventListener(
   'click', () => {
-    setTimeout(() => {  send('AT+BATT?'); }, 200);
+    // sendcmd('AT+BATT?');
+    flag = 'battery_voltage';
+    setTimeout(() => {  sendcmd('AT+BATT?'); }, 200);
   }
 );
 
 
 sendForm.addEventListener('submit', (event) => {
   event.preventDefault();
-
+  flag = 'None';
   send(inputField.value);
 
   // inputField.value = '';
